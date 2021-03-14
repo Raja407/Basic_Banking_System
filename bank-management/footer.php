692341e9479640ac7ec26b0dac3275077a7a35cb
Raja407 <65301430+Raja407@users.noreply.github.com>
2021-03-15 00:04:13 +0530

    Add files via upload

---
+++ bank-management/footer.php
@@ -0,0 +1,13 @@
+<footer class="container-fluid text-center footr">
+	<div class="container">
+  <p class="footer-copy">Developed by <a class="footer-made" href="https://www.linkedin.com/in/rajamanickamweb" target="_blank">Rajamanickam</a> </p>
+  <!-- <div class="footer-social">
+  	<i class="fa fa-facebook"></i>
+  	<i class="fa fa-twitter"></i>
+  	<i class="fa fa-instagram"></i>
+  </div> -->
+  </div>
+</footer>
+
+</body>
+</html>
